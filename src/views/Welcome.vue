6fec4f0573eba0921c9d4dabde223d309c4996bc
epkeskitalo <esa-pekka.keskitalo@helsinki.fi>
2018-10-31 12:51:50 +0200

    Update Welcome.vue
    Changes to text in Lead slot.

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -5,14 +5,12 @@
 		</template>
 
 		<template slot="lead">
-			Welcome to Qvain, the metadata record editor of the ATT project.
+			Welcome to Qvain, the dataset metadata editor. Qvain is part of <a href="https://www.fairdata.fi">Fairdata Services.</a>
 		</template>
 
 		<hr class="my-4">
 
-		<p>To use the metadata editor, you need an account with the ATT project. You can create a local account or log in via Haka.</p>
-		<p>With the toolbar above you can <router-link to="/tabui">create a new record</router-link> or <router-link to="/list">list your records</router-link>.</p>
+		<p>To use the metadata editor, you need to login using your Haka account. Other authentication methods will be added in the future.</p>
 
-		<p><router-link class="btn btn-info btn-lg" to="/tabui" role="button">Create one now!</router-link></p>
 	</b-jumbotron>
 </template>
